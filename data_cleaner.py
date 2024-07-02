e4dd662ccce580b101398ef3d2ff08ef053cfb7d
scush989898 <marcoxd01@hotmail.com>
2024-07-01 22:14:36 -0300

    update: update data_cleaner and graph generator scripts, update weka results files

--- data_cleaner.py
+++ data_cleaner.py
@@ -2,15 +2,15 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn import preprocessing
 import missingno as msno
+from imblearn.combine import SMOTETomek
 
 df = pd.read_csv("database/origin/healthcare-dataset-stroke-data.csv")
 
 print(df.head())
 print(df.isna().sum(), "Dados ausentes")
-print(msno.bar(df))
+msno.bar(df)
 
 imputer = SimpleImputer(strategy="mean")
-imputer.fit_transform
 df[["bmi"]] = imputer.fit_transform(df[["bmi"]])
 
 print(df.isna().sum())
@@ -21,4 +21,19 @@ print(df.head())
 
 df["stroke"] = df["stroke"].replace({1: "YES", 0: "NO"})
 
-df.to_csv("database/formated/encoded_df.csv", index=False)
+X = df.drop("stroke", axis=1)
+y = df["stroke"]
+
+os = SMOTETomek(sampling_strategy=0.6)
+X_res, y_res = os.fit_resample(X, y)
+
+df_res = pd.concat(
+    [
+        pd.DataFrame(X_res, columns=X.columns),
+        pd.DataFrame(y_res, columns=["stroke"]),
+    ],
+    axis=1,
+)
+
+df_res.to_csv("database/formated/encoded_df.csv", index=False)
+print(df_res["stroke"].value_counts())
--- graph_generator.py
+++ graph_generator.py
@@ -26,57 +26,6 @@ def process_summary_df(summary_df):
     return summary_df
 
 
-def plot_execution_time(execution_times):
-    plt.figure(figsize=(14, 7))
-    execution_times_df = pd.DataFrame(
-        list(execution_times.items()),
-        columns=["Algorithm", "Time Taken (seconds)"],
-    )
-    sns.barplot(
-        x="Algorithm", y="Time Taken (seconds)", data=execution_times_df
-    )
-    plt.title("Execution Time Comparison")
-    plt.xlabel("Algorithm")
-    plt.ylabel("Time Taken (seconds)")
-    plt.xticks(rotation=45)
-    plt.show()
-
-
-def plot_accuracy_error(accuracy_error):
-    plt.figure(figsize=(14, 7))
-
-    # Precisão
-    plt.subplot(121)
-    sns.barplot(x="Algorithm", y="Accuracy", data=accuracy_error)
-    plt.title("Accuracy Comparison")
-    plt.xlabel("Algorithm")
-    plt.ylabel("Accuracy")
-    plt.xticks(rotation=45)
-
-    # Erro
-    plt.subplot(122)
-    sns.barplot(x="Algorithm", y="Error", data=accuracy_error)
-    plt.title("Error Comparison")
-    plt.xlabel("Algorithm")
-    plt.ylabel("Error")
-    plt.xticks(rotation=45)
-
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_combined_confusion_matrix(confusion_matrices):
-    combined_matrix = pd.concat(
-        [df for df in confusion_matrices.values()], ignore_index=True
-    )
-    plt.figure(figsize=(14, 7))
-    sns.heatmap(combined_matrix, annot=True, fmt="d", cmap="Blues")
-    plt.title("Combined Confusion Matrix")
-    plt.ylabel("Actual")
-    plt.xlabel("Predicted")
-    plt.show()
-
-
 algorithm_names = [
     "1-decision_table",
     "2-jrip",
@@ -92,68 +41,46 @@ algorithm_names = [
     "12-rep_tree",
 ]
 
-execution_times = {}
-accuracy_error = {"Algorithm": [], "Accuracy": [], "Error": []}
+summaries = {}
 confusion_matrices = {}
 
-for algorithm_name in algorithm_names:
-    summary_df, confusion_matrix_df = load_data(algorithm_name)
+for algorithm in algorithm_names:
+    summary_df, confusion_matrix_df = load_data(algorithm)
     summary_df = process_summary_df(summary_df)
+    summaries[algorithm] = summary_df
+    confusion_matrices[algorithm] = confusion_matrix_df
 
-    time_taken_row = summary_df.loc[
-        summary_df["Métrica"] == "Time taken (in seconds)"
-    ]
-    if not time_taken_row.empty:
-        execution_times[algorithm_name] = time_taken_row["Valor"].values[0]
-    else:
-        print(f"No 'Time taken' entry found for {algorithm_name}. Skipping.")
-        continue
+metrics = [
+    metric.strip()
+    for metric in summaries[algorithm_names[0]]["Métrica"].tolist()
+]
 
-    accuracy_row = summary_df.loc[
-        summary_df["Métrica"] == "Correctly Classified Instances"
-    ]
-    if not accuracy_row.empty:
-        accuracy = (
-            accuracy_row["Porcentagem sobre o total"]
-            .values[0]
-            .replace("%", "")
-            .replace(",", ".")
-        )
-        accuracy_error.loc[len(accuracy_error)] = [
-            algorithm_name,
-            float(accuracy),
-            None,
-        ]  # None para Error, pois estamos apenas lidando com Accuracy aqui
-    else:
-        print(f"No 'Accuracy' entry found for {algorithm_name}. Skipping.")
-        continue
+for metric in metrics:
+    plt.figure(figsize=(14, 8))
+    values = []
+    for algorithm in algorithm_names:
+        metric_value = summaries[algorithm].loc[
+            summaries[algorithm]["Métrica"].str.strip() == metric, "Valor"
+        ]
+        if not metric_value.empty:
+            values.append(metric_value.values[0])
+        else:
+            print(
+                f"""Métrica '{metric}' não encontrada
+                para o algoritmo '{algorithm}'"""
+            )
+            values.append(0)
+    sns.barplot(x=algorithm_names, y=values, palette="Blues_d")
+    plt.title(f"{metric}")
+    plt.ylabel("Value")
+    plt.xlabel("Algorithm")
+    plt.xticks(rotation=45)
+    plt.show()
 
-    error_row = summary_df.loc[
-        summary_df["Métrica"] == "Incorrectly Classified Instances"
-    ]
-    if not error_row.empty:
-        error = (
-            error_row["Porcentagem sobre o total"]
-            .values[0]
-            .replace("%", "")
-            .replace(",", ".")
-        )
-        accuracy_error.loc[len(accuracy_error)] = [
-            algorithm_name,
-            None,
-            float(error),
-        ]  # None para Accuracy, pois estamos apenas lidando com Error aqui
-    else:
-        print(f"No 'Error' entry found for {algorithm_name}. Skipping.")
-        continue
-
-    confusion_matrices[algorithm_name] = confusion_matrix_df
-
-# Plotar o tempo de execução
-plot_execution_time(execution_times)
-
-# Plotar precisão e erros
-plot_accuracy_error(pd.DataFrame(accuracy_error))
-
-# Plotar matriz de confusão combinada
-plot_combined_confusion_matrix(confusion_matrices)
+for algorithm, confusion_matrix_df in confusion_matrices.items():
+    plt.figure(figsize=(10, 6))
+    sns.heatmap(confusion_matrix_df, annot=True, fmt="d", cmap="Blues")
+    plt.title(f"Consusion Matrix - {algorithm}")
+    plt.ylabel("Real")
+    plt.xlabel("Predicted Value")
+    plt.show()
